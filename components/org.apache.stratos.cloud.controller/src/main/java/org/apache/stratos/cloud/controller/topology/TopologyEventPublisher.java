2150658203d46cb44441e3c747503350b1e92bb0
gayan <gayan@puppet.gayan.org>
2014-09-22 14:28:42 +0530

    messaging model changes and MQTT updates

--- components/org.apache.stratos.cloud.controller/src/main/java/org/apache/stratos/cloud/controller/topology/TopologyEventPublisher.java
+++ components/org.apache.stratos.cloud.controller/src/main/java/org/apache/stratos/cloud/controller/topology/TopologyEventPublisher.java
@@ -8,9 +8,9 @@ package org.apache.stratos.cloud.controller.topology;
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -115,7 +115,7 @@ public class TopologyEventPublisher {
 		                                          new ClusterRemovedEvent(ctxt.getCartridgeType(),
 		                                                                  ctxt.getClusterId(),
 		                                                                  deploymentPolicy,
-		                                                                  ctxt.isLbCluster());
+		                                                                  ctxt.isLbCluster(),ctxt.isKubernetesCluster());
 
 		if (log.isInfoEnabled()) {
 			log.info(String.format("Publishing cluster removed event: [service] %s [cluster] %s",
